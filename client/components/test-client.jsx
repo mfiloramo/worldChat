431a2c962b85e520d503df39d9a8861c89458c51
mfiloramo <mlfiloramo@gmail.com>
2022-08-17 10:35:03 -0400

    reformatted styles.css

--- client/components/test-client.jsx
+++ client/components/test-client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import axios from 'axios';
-import * as io from '../../node_modules/socket.io/client-dist/socket.io';
+// import axios from 'axios';
+// import * as io from '../../node_modules/socket.io/client-dist/socket.io';
 
 
 function TestClient() {
@@ -13,12 +13,15 @@ function TestClient() {
       <meta name={"viewport"} content={"width=device-width, initial-scale=1.0"} />
       <title>Test Client</title>
       <link rel={"stylesheet"} type={"text/css"} href={"../stylesheets/styles.css"} />
-      <box className={'box'}>This is a Tailwind box</box>
+      <div className={'box'}>This is a Tailwind box</div>
       <br/>
       <button className={'btn'} onClick={() => {console.log('hello there!')}}>This is a Tailwind button</button>
     </div>
   )
 }
 
+/**
+ react-dom.development.js:86 Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
+ */
 
 export default TestClient;
