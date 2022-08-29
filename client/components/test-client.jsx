01bf2cac29bec3e9073f3a2ae238dcebe707055f
mfiloramo <mlfiloramo@gmail.com>
2022-08-28 20:08:16 -0400

    updated interface

--- client/components/test-client.jsx
+++ client/components/test-client.jsx
@@ -29,14 +29,6 @@ function TestClient() {
       <button className={"btn"} onClick={() => {console.log('hello there!')}}>This is a Tailwind button</button>
     </div>
 
-      <form action="/action_page.php">
-        <label htmlFor="fname">First name:</label>
-        <input type="text" id="fname" name="fname"><br><br>
-        <label htmlFor="lname">Last name:</label>
-        <input type="text" id="lname" name="lname"><br><br>
-        <input type="submit" value="Submit">
-      </form>
-
     </div>
   )
 }
